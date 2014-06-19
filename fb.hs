3b2a1ca54b2987f39417afbe374099c5cea91d1c
Jeremy <jeremymwatson@gmail.com>
2014-06-19 10:33:29 -0400

    compiles

--- fb.hs
+++ fb.hs
@@ -3,14 +3,18 @@ import System.Environment
 import Data.List
 
 -- broken, need to research do I/O, fiStr line might be culprit
+
+  --fibi :: Int-> Int-> Int -> String
+fibi fn sn num  
+  | (mod (fn*sn) num) == 0  = "FB"
+  | mod fn num        == 0  = "F"
+  | mod sn num        == 0  = "B"
+  | otherwise               = show num
+  
+readInt :: [Char] -> Int
+readInt x = read x
+
 main = do
     args <- getArgs
-    fiStr <- map (read (words readFile (Head args)) :: Int)
-   fibi :: Int-> Int-> Int -> String
-   fibi fn sn num 
-      | (mod (fn*sn) num) == 0  = "FB"
-      | mod fn num        == 0  = "F"
-      | mod sn num        == 0  = "B"
-      | otherwise               = show num
-   chew x = case xs of [] -> []
-                        i:j:k:_ -> print (map (fibi i j [1..k]))
+    fiStr <- readFile (head args)
+    print $ map readInt (take 3 (words fiStr)) 
