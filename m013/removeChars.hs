e3065c4ca50de58974a58370553a03ab72d90d1f
Jeremy <jeremymwatson@gmail.com>
2014-06-23 15:33:38 -0400

    copy paste programming

--- m013/removeChars.hs
+++ m013/removeChars.hs
@@ -1,2 +1,23 @@
 --fold remove
 foldr (\x y -> if (x `elem` "abc") then y else x:y) "" "Thabis sabecncteanbce"
+
+import Data.List
+import Data.Function
+import Data.List.Split
+import System.Environment
+
+--junk below for reference
+rmstr :: [Char] -> [Char] -> [Char]
+
+rmstr str filtr = foldr (\x y -> if (x `elem` filtr) then y else x:y) "" str
+chew :: [String] -> String
+
+chew [] = ""
+chew (hd:[]) = if flipr (words hd)
+chew (hd:tl) = (flipr (words hd)) ++ "\n" ++ (chew tl)
+
+
+main = do
+    args <- getArgs
+    fiStr <- readFile (head args)
+    putStrLn $ chew $ map splitOn ", " $ splitOn "\n" fiStr
