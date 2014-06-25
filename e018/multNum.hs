0e64b2e360ceb46b76b20f258de4de891d88d47c
jeremy <jeremy@watsonly.com>
2014-06-24 21:28:23 -0400

    finish easy 18

---
+++ e018/multNum.hs
@@ -0,0 +1,22 @@
+import Data.List
+import Data.Function
+import Data.List.Split
+import System.Environment
+
+readInt :: [Char] -> Int
+readInt x = read x
+
+biggest :: Int -> Int -> String
+biggest num tes = show $ head [x|x<-[num*1,num*2..],x > tes]
+
+chew :: [[String]] -> String
+
+chew [] = ""
+chew ((hd:hd2:_):[]) = biggest (readInt hd2) (readInt hd)
+chew ((hd:hd2:_):tl) = biggest (readInt hd2) (readInt hd) ++ "\n" ++ (chew tl)
+
+
+main = do
+      args <- getArgs
+      fiStr <- readFile (head args)
+      putStrLn $ chew $ map (splitOn ",") $ endBy "\n" fiStr
