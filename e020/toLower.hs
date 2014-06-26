3ed26f29eb968a846a56e17baab60ce250c1dcd0
Jeremy W <jeremymwatson@gmail.com>
2014-06-26 07:23:16 -0400

    done with e020

--- e020/toLower.hs
+++ e020/toLower.hs
@@ -1,9 +1,10 @@
 import Data.List as L
 import Data.Char as C
 import System.Environment
-import Data.Text
+--import Data.Text
 
 main = do
     args <- getArgs
     fiStr <- readFile (L.head args)
-    putStrLn $ strip $ L.map C.toLower fiStr
+   -- putStrLn $ strip $ map C.toLower fiStr
+    putStrLn $ map C.toLower fiStr
