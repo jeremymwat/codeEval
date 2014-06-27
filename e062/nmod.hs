1f4fbc51a3c1c7f650af76546601646d3fa0ce8d
Jeremy <jeremymwatson@gmail.com>
2014-06-27 09:53:05 -0400

    create nmodm

---
+++ e062/nmod.hs
@@ -0,0 +1,21 @@
+import Data.List
+import Data.Function
+import Data.List.Split
+import System.Environment
+import Data.Char
+
+readInt :: String -> Int
+readInt x = read x
+
+
+mth :: [String] -> String
+
+nmodm (n:m:_) = head [ n-m*x | x <- [1,2..], (n -(m*x))<m]
+
+
+
+main = do
+    args <- getArgs
+    fiStr <- readFile (head args)
+--    putStrLn $ unlines $  (lines fiStr)
+
