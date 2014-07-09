14cbd2db933603ea8e74d2aab5aaa5bcef083f8f
jeremy <jeremy@watsonly.com>
2014-07-09 16:47:15 -0400

    done e023

--- e023/multTable.hs
+++ e023/multTable.hs
@@ -2,12 +2,15 @@ import Data.List
 import Data.Function
 import Data.List.Split
 
--- get strlen of each element, take from spaces, append
 spaces = "    "
 lst = [map (\y -> show ((*) x y)) [1,2..12] | x<- [1,2..12]]
 
-spcApp str
+spcApp :: [String] -> [String]
 
+spcApp str = case str of 
+             hd:[]      -> [hd]
+             hd:tl      -> hd:(take (4 - (length (head tl))) spaces):(spcApp tl)
+             _          -> []
 
 main = do
-    putStrLn $ unlines $ map concat $ map (intersperse "    ") lst
+    putStrLn $ unlines $ map concat $ map spcApp lst
