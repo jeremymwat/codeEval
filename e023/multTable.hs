8a0cdaf1a69c48d34d435e24f3066caf12ee41c4
Jeremy W <jeremymwatson@gmail.com>
2014-07-09 16:14:54 -0400

    start last func

--- e023/multTable.hs
+++ e023/multTable.hs
@@ -1,10 +1,13 @@
 import Data.List
 import Data.Function
 import Data.List.Split
-import System.Environment
 
--- map interleave 4 spaces over final product
+-- get strlen of each element, take from spaces, append
+spaces = "    "
 lst = [map (\y -> show ((*) x y)) [1,2..12] | x<- [1,2..12]]
+
+spcApp str
+
 
 main = do
     putStrLn $ unlines $ map concat $ map (intersperse "    ") lst
