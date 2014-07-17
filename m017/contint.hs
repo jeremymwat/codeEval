386ca1296865a8a4075cc71fab9435432dbe65e2
Jeremy <jeremymwatson@gmail.com>
2014-07-17 09:57:50 -0400

    finish m017

--- m017/contint.hs
+++ m017/contint.hs
@@ -3,7 +3,7 @@ import Data.List.Split
 
 long :: [Int] -> String
 long lst = show ln
-              where (_,ln) = foldr (\x (y,n) -> if (x + y) < 0 then (0,n) else ((x + y),(max n (x+y)))) (0,0) lst
+              where (_,ln) = foldl (\(y,n) x -> if (x + y) < 0 then (0,(max n x)) else ((x + y),(max n (x+y)))) (0,head lst) lst
 
 rd :: String -> Int
 rd x = read x
