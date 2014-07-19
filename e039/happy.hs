5ab9869af7baa8f3c2fd568bad6b3d68e9b6f474
jeremy <jeremy@watsonly.com>
2014-07-19 18:33:09 -0400

    finish e039

--- e039/happy.hs
+++ e039/happy.hs
@@ -1,14 +1,18 @@
 import System.Environment
 import Data.Char
 
--- take arg string, break in to lines, remove commas, read from string to int
-lis :: String -> [Int]
-lis lst = map (\x -> map rd $ splitOn "," x) (lines lst)
-
 sumsq :: String -> String
 sumsq str = show $ foldr (\x y -> (digitToInt x)^2 + y) 0 str
+
+process (str,lst) 
+    | str `elem` lst      = "0"
+    | smq == "1"          = "1"
+    | otherwise           = process (smq, str:lst)
+      where smq = sumsq str
+
+
 
 main = do
     args <- getArgs
     fiStr <- readFile (head args)
-    putStrLn $ unlines $ process (lines fiStr)
+    putStrLn $ unlines $ map (\x -> process (x,[])) (lines fiStr)
