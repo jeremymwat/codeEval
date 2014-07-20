538b1385c2ddf0c1124468e09a94969c371e017a
jeremy <jeremy@watsonly.com>
2014-07-19 23:02:21 -0400

    add e083

--- e083/bstrings.hs
+++ e083/bstrings.hs
@@ -1,11 +1,14 @@
 import System.Environment
 import Data.Char
+import Data.List
+
+parseS :: String -> [Int]
+parseS str = map length $ sortBy (\x y -> compare (length y) (length x)) $ groupBy (==) $ sort str
 
 
-
-
+process strs = map (\x -> show . sum $ zipWith (*) [26,25..1] (parseS x)) strs
 
 main = do
     args <- getArgs
     fiStr <- readFile (head args)
-    putStrLn $ unlines $ process (map (toLower $ filter isAlpha) $ lines fiStr)
+    putStrLn $ unlines $ process (map (\x -> map toLower $ filter isAlpha x) $ lines fiStr)
